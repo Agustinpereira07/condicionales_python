56cd2565a9f386352f08ffed39b0110a6ed5da42
Agustinpereira07 <101513567+Agustinpereira07@users.noreply.github.com>
2022-04-07 15:51:56 -0300

    Add files via upload

---
+++ tarea2.py
@@ -0,0 +1,30 @@
+texto_1 = "7"
+
+texto_2 = "5"
+
+
+if texto_1 > texto_2:
+    print("texto_1 es mayor que texto_2")
+
+    texto_1 = 7
+
+    texto_2 = 5
+
+    if texto_1 > texto_2:
+
+        print("ca es mayor numericamente a texto_2 ")
+
+elif texto_1 < texto_2:
+
+    print(" texto_1 es menor que texto_2")
+
+    texto_1 = 7
+
+    texto_2 = 5
+
+    if texto_1 < texto_2:
+
+        print("texto_1 es menor numericamente a texto_2")
+
+else: 
+    print("no se a encontrado solucion")
